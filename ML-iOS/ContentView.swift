dabccf1cc87973e31c07c853d8710c45dc24cfe9
Vimlesh Kumar <vimlesh826@gmail.com>
2022-08-20 16:50:41 +0530

    Initial Commit

---
+++ ML-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ML-iOS
+//
+//  Created by Vimlesh Kumar on 20/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ML-iOS/ML_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ML_iOSApp.swift
+//  ML-iOS
+//
+//  Created by Vimlesh Kumar on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ML_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
